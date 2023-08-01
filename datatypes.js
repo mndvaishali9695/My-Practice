cc81615f27e152a2006aa055abd1ef61a55d7327
mndvaishali9695 <mvaishali221@gmail.com it config --global user.email>
2023-08-01 07:36:37 -0700

    variables in js

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// JavaScript has 8 Datatypes
+//  String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+//Object
+//string :'vaishali'/"vaishali"/'2020': use when value is alphumeric/number/as. a
+
+
+var name
+
+
+
+
+
+
+
+
+
+// 7
---
+++ variables.js
@@ -0,0 +1,18 @@
+//variables.:is Containers for Storing a data(any data types);g home
+//we can  declared variables using 4way default .let,var,const
+//syntax:
+
+
+
+
+let a;//declr
+console.log(a);
+a = 20; // deffinetion
+console.log(a);
+
+let b = 50;// decl-deff
+
+//vaiable name: space npt allowed,special charters not allowed,
+// first letter of wil shpooold alphabet only
+
+let mathsMarks = 60;
