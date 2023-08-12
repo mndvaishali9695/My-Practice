a42997ed640c061c6fe46d5f2d81c54ea1e46efc
mndvaishali9695 <mvaishali221@gmail.com it config --global user.email>
2023-08-12 05:01:22 -0700

    stringmethods

--- stringmethods.js
+++ stringmethods.js
@@ -1,7 +1,7 @@
 
 // string methods
 
-let text = ' I live in Mumbai';
+let text = 'I live in Mumbai';
 let  textLenght = text. length;
  console.log('textLenght',textLenght);
 
@@ -10,3 +10,32 @@ let  textLenght = text. length;
 
  let a = text.slice(3,7);
 console.log('a>>>',a);
+
+let  b = text.slice(-10,-5)  //slice(start[exclusive], end[inclusive])
+console.log(b);
+
+  console.log(text.substring(6,-9)) //i live -9=0 , so 6 will be end index(0,6)
+
+  //substr(start index, length)
+
+  console.log(text.substr(2,9));
+
+  // replece
+
+  let r = 'Pune method replaces  a specified pune value with another pune value in a stringmethod replaces a specified value with another value in a string';
+
+  let q = r.replace(/pune/gi,'mumbai');
+  console.log(q);
+
+  let p = r.replaceAll('pune','mumbai');
+  console.log(p);
+
+  //concat (or +)
+  let FN = 'vaishali';
+  let LN =  'MAHADIK';
+  let f_name = FN +" " + LN;
+  console.log(f_name);
+
+ 
+  let F_name = FN.concat(' ', LN,' ', 'mahadik');
+  console.log(F_name);
